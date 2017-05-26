65c7e51ec6a37a0c159101a2bb95be443b87d8a5
Dripmaster <son342@gmail.com>
2017-05-27 02:15:53 +0900

    make Canvus and scene System

---
+++ Assets/Script/readysceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class readysceneManager : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject settingPanel;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void settingButtonClicked() {
+        cleanScreen();
+        settingPanel.SetActive(true);
+    }
+
+    public void collectionButtonClicked()
+    {
+        cleanScreen();
+
+    }
+
+    public void diceButtonClicked()
+    {
+
+        cleanScreen();
+    }
+
+    public void ballcollButtonClicked()
+    {
+        cleanScreen();
+    }
+
+    public void startButtonClicked() {
+
+        Debug.Log("스타트");
+    
+    }
+
+    void cleanScreen() {
+        mainPanel.gameObject.SetActive(false);
+    }
+}
